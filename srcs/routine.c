f04e098bf365b7f57c3ece1ea45778b1d8c2683f
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-03-29 15:22:13 +0200

    cleanup

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: alemarch <alemarch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 10:43:22 by alemarch          #+#    #+#             */
-/*   Updated: 2022/03/29 14:51:09 by alemarch         ###   ########.fr       */
+/*   Updated: 2022/03/29 15:20:56 by alemarch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@ void	putstatus(int i, char *s)
 	printf("%u %i %s\n", get_timenow(), i, s);
 }
 
-void	tryeat(t_philo *philo, int amount_eaten, long t_without_eating,
-		long last_eat)
+void	tryeat(t_philo *philo, int amount_eaten, long last_eat)
 {
 	pthread_mutex_lock(&philo->lfork);
 	putstatus(philo->id, "has taken a fork");
@@ -29,14 +28,13 @@ void	tryeat(t_philo *philo, int amount_eaten, long t_without_eating,
 	pthread_mutex_unlock(&philo->rfork);
 	pthread_mutex_unlock(&philo->lfork);
 	amount_eaten++;
-	t_without_eating = 0;
-	last_eat = get_timenow();
+	if (get_timenow() - last_eat < philo->time[DIE])
+		last_eat = get_timenow();
 }
 
-int	routine_helper(t_philo *philo, unsigned int t_without_eating
-		, unsigned int last_eat)
+int	routine_helper(t_philo *philo, unsigned int last_eat)
 {
-	if (t_without_eating >= philo->time[DIE])
+	if (get_timenow() - last_eat >= philo->time[DIE])
 	{
 		philo->status = DEAD;
 		pthread_mutex_unlock(&philo->status_mutex);
@@ -50,7 +48,7 @@ int	routine_helper(t_philo *philo, unsigned int t_without_eating
 		return (1);
 	}
 	pthread_mutex_unlock(&philo->status_mutex);
-	tryeat(philo, philo->amount_eaten, t_without_eating, last_eat);
+	tryeat(philo, philo->amount_eaten, last_eat);
 	putstatus(philo->id, "is sleeping");
 	usleep(philo->time[SLEEP] * 1000);
 	putstatus(philo->id, "is thinking");
@@ -60,21 +58,16 @@ int	routine_helper(t_philo *philo, unsigned int t_without_eating
 void	*routine(void *args)
 {
 	t_philo	*philo;
-	long	t_without_eating;
 	long	last_eat;
 
 	philo = (t_philo *)args;
-	t_without_eating = 0;
 	last_eat = philo->timestamp;
 	if (philo->id % 2 == 0)
-	{
 		usleep(philo->time[EAT] / 10);
-		t_without_eating += philo->time[EAT] / 10;
-	}
 	while (1)
 	{
 		pthread_mutex_lock(&philo->status_mutex);
-		routine_helper(philo, t_without_eating, last_eat);
+		routine_helper(philo, last_eat);
 	}
 	return (NULL);
 }
